51d40699d646d36a20c9ea6762ba6c3f87581e8c
与时俱进 <1481999271@qq.com>
2017-02-09 10:29:11 +0800

    Activity 基类

---
+++ app/src/main/java/com/example/gitapp/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.example.gitapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by 李小兵 on 2017/2/9.
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+
+    }
+}
